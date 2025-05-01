db2f9afadb72f01fc54477ced42060e4740f9703
Ben Grimes <bgrimes@gmail.com>
2025-05-01 17:15:55 -0600

    Fix double work issue

--- src/App.tsx
+++ src/App.tsx
@@ -249,7 +249,7 @@ function App() {
                   <div className="text-sm">
                     {feedback}
                     {showAnswer && (feedback.includes("Incorrect") || feedback.includes("Skipped")) && (
-                      <div className="mt-1">The correct answer is:<br /><strong>{correctAnswer}</strong></div>
+                      <div className="mt-1"><strong>{correctAnswer}</strong></div>
                     )}
                   </div>
                 </div>
